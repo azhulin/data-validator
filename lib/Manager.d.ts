5eda63046d1ac9d655b2b3ed44f501b33ec030bd
Alex Zhulin <alex.zhulin@ffwagency.com>
2021-05-06 14:57:07 +0300

    Manager extends PluginManager.

--- lib/Manager.d.ts
+++ lib/Manager.d.ts
@@ -4,21 +4,11 @@ import PluginManager from "@azhulin/plugin-manager";
 /**
  * The data handler manager.
  */
-export default class Manager {
-    /**
-     * The handlers info.
-     */
-    get info(): Record<string, {
-        path: string;
-    }>;
-    /**
-     * The plugin manager.
-     */
-    protected pluginManager: PluginManager;
+export default class Manager extends PluginManager {
     /**
      * Constructor for the Manager object.
      */
-    constructor(path?: string | string[], dirname?: string, match?: string | RegExp | (string | RegExp)[]);
+    constructor();
     /**
      * Returns a data handler for the specified data type or data definition.
      */
--- src/Manager.ts
+++ src/Manager.ts
@@ -6,27 +6,14 @@ import PluginManager from "@azhulin/plugin-manager"
 /**
  * The data handler manager.
  */
-export default class Manager {
-
-  /**
-   * The handlers info.
-   */
-  public get info(): Record<string, { path: string }> {
-    return this.pluginManager.info
-  }
-
-  /**
-   * The plugin manager.
-   */
-  protected pluginManager: PluginManager
+export default class Manager extends PluginManager {
 
   /**
    * Constructor for the Manager object.
    */
-  public constructor(path: string | string[] = [], dirname: string = "", match?: string | RegExp | (string | RegExp)[]) {
-    this.pluginManager = new PluginManager(Handler)
-      .register("handler", __dirname)
-      .register(path, dirname, match)
+  public constructor() {
+    super(Handler)
+    this.register("handler", __dirname)
   }
 
   /**
@@ -36,9 +23,8 @@ export default class Manager {
   public handler(type: string, config?: Config): Handler
   public handler(type: string | Definition, config?: Config): Handler {
     ({ type, ...config } = Handler.definitionNormalize(type, config))
-    const { pluginManager } = this
-    const settings: Settings = { config, pluginManager }
-    return pluginManager.instance(type, settings)
+    const settings: Settings = { config, pluginManager: this }
+    return this.instance(type, settings)
   }
 
 }
